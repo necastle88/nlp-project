7b441b894ed00a56a7da7c8b691634d50d794550
necastle88 <devLifeNC@gmail.com>
2021-03-26 23:40:18 -0700

    created production and development webpack configs

--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,23 @@
 var path = require('path')
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
+const port = 8000
 
-app.use(express.static('src/client'))
+app.use(express.static('dist'))
 
 console.log(__dirname)
 
-app.get('/', function (req, res) {
-    res.sendFile('/client/views/index.html', { root: __dirname + '/..' })
+app.get("/", function (req, res) {
+    res.sendFile('index.html', { root: '../../dist' })
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8000, function () {
-    console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+    console.log('Example app listening on port 8000!')
 })
-
+/*
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+*/
